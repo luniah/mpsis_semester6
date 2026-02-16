5c034c53c40163bc3b83e92931ebc45f6ba3744b
luniah <pozharskaya2005@mail.ru>
2026-02-16 18:47:47 +0000

    lab1 done

---
+++ mpsis.sim/sim_1/behav/xsim/xsim.dir/lab_01_tb_fulladder32_behav/obj/xsim_1.c
@@ -0,0 +1,152 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2020 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+#if defined(_WIN32)
+ #include "stdio.h"
+ #define IKI_DLLESPEC __declspec(dllimport)
+#else
+ #define IKI_DLLESPEC
+#endif
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2020 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+#if defined(_WIN32)
+ #include "stdio.h"
+ #define IKI_DLLESPEC __declspec(dllimport)
+#else
+ #define IKI_DLLESPEC
+#endif
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+IKI_DLLESPEC extern void execute_2(char*, char *);
+IKI_DLLESPEC extern void execute_44(char*, char *);
+IKI_DLLESPEC extern void execute_377(char*, char *);
+IKI_DLLESPEC extern void svlog_sampling_process_execute(char*, char*, char*);
+IKI_DLLESPEC extern void sequence_expr_m_43b986c1563b57e0_eaca600b_1(char*, char *);
+IKI_DLLESPEC extern void vlog_sv_sequence_execute_0 (char*, char*, char*);
+IKI_DLLESPEC extern void assertion_action_m_43b986c1563b57e0_eaca600b_1(char*, char *);
+IKI_DLLESPEC extern void execute_382(char*, char *);
+IKI_DLLESPEC extern void execute_383(char*, char *);
+IKI_DLLESPEC extern void execute_384(char*, char *);
+IKI_DLLESPEC extern void execute_54(char*, char *);
+IKI_DLLESPEC extern void execute_55(char*, char *);
+IKI_DLLESPEC extern void execute_368(char*, char *);
+IKI_DLLESPEC extern void execute_369(char*, char *);
+IKI_DLLESPEC extern void execute_370(char*, char *);
+IKI_DLLESPEC extern void execute_371(char*, char *);
+IKI_DLLESPEC extern void execute_372(char*, char *);
+IKI_DLLESPEC extern void execute_373(char*, char *);
+IKI_DLLESPEC extern void execute_374(char*, char *);
+IKI_DLLESPEC extern void execute_375(char*, char *);
+IKI_DLLESPEC extern void execute_376(char*, char *);
+IKI_DLLESPEC extern void execute_56(char*, char *);
+IKI_DLLESPEC extern void execute_57(char*, char *);
+IKI_DLLESPEC extern void execute_90(char*, char *);
+IKI_DLLESPEC extern void execute_91(char*, char *);
+IKI_DLLESPEC extern void execute_92(char*, char *);
+IKI_DLLESPEC extern void execute_93(char*, char *);
+IKI_DLLESPEC extern void execute_94(char*, char *);
+IKI_DLLESPEC extern void execute_58(char*, char *);
+IKI_DLLESPEC extern void execute_59(char*, char *);
+IKI_DLLESPEC extern void execute_60(char*, char *);
+IKI_DLLESPEC extern void execute_61(char*, char *);
+IKI_DLLESPEC extern void execute_62(char*, char *);
+IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_for_reg(char*, char*, char*);
+IKI_DLLESPEC extern void execute_64(char*, char *);
+IKI_DLLESPEC extern void execute_65(char*, char *);
+IKI_DLLESPEC extern void execute_50(char*, char *);
+IKI_DLLESPEC extern void execute_51(char*, char *);
+IKI_DLLESPEC extern void execute_52(char*, char *);
+IKI_DLLESPEC extern void execute_53(char*, char *);
+IKI_DLLESPEC extern void execute_385(char*, char *);
+IKI_DLLESPEC extern void execute_386(char*, char *);
+IKI_DLLESPEC extern void execute_387(char*, char *);
+IKI_DLLESPEC extern void execute_388(char*, char *);
+IKI_DLLESPEC extern void execute_389(char*, char *);
+IKI_DLLESPEC extern void execute_390(char*, char *);
+IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+IKI_DLLESPEC extern void transaction_5(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void vlog_transfunc_eventcallback_2state(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[49] = {(funcp)execute_2, (funcp)execute_44, (funcp)execute_377, (funcp)svlog_sampling_process_execute, (funcp)sequence_expr_m_43b986c1563b57e0_eaca600b_1, (funcp)vlog_sv_sequence_execute_0 , (funcp)assertion_action_m_43b986c1563b57e0_eaca600b_1, (funcp)execute_382, (funcp)execute_383, (funcp)execute_384, (funcp)execute_54, (funcp)execute_55, (funcp)execute_368, (funcp)execute_369, (funcp)execute_370, (funcp)execute_371, (funcp)execute_372, (funcp)execute_373, (funcp)execute_374, (funcp)execute_375, (funcp)execute_376, (funcp)execute_56, (funcp)execute_57, (funcp)execute_90, (funcp)execute_91, (funcp)execute_92, (funcp)execute_93, (funcp)execute_94, (funcp)execute_58, (funcp)execute_59, (funcp)execute_60, (funcp)execute_61, (funcp)execute_62, (funcp)vlog_simple_process_execute_0_fast_for_reg, (funcp)execute_64, (funcp)execute_65, (funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_385, (funcp)execute_386, (funcp)execute_387, (funcp)execute_388, (funcp)execute_389, (funcp)execute_390, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_5, (funcp)vlog_transfunc_eventcallback_2state};
+const int NumRelocateId= 49;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/lab_01_tb_fulladder32_behav/xsim.reloc",  (void **)funcTab, 49);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/lab_01_tb_fulladder32_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/lab_01_tb_fulladder32_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_xsimdir_location_if_remapped(argc, argv)  ;
+    iki_set_sv_type_file_path_name("xsim.dir/lab_01_tb_fulladder32_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/lab_01_tb_fulladder32_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/lab_01_tb_fulladder32_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
--- mpsis.srcs/sources_1/new/fulladder32.sv
+++ mpsis.srcs/sources_1/new/fulladder32.sv
@@ -1,11 +1,25 @@
 module fulladder32(
-    input  logic [31:0] a_i,
-    input  logic [31:0] b_i,
-    input  logic        carry_i,
-    output logic [31:0] sum_o,
-    output logic        carry_o
+  input  logic [31:0] a_i,
+  input  logic [31:0] b_i,
+  input  logic       carry_i,
+  output logic [31:0] sum_o,
+  output logic       carry_o
 );
 
+logic [7:0] carry_ii;
+logic [7:0] carry_oo;
 
+assign carry_ii[0] = carry_i;
+assign carry_o = carry_oo[7];
+
+fulladder4 adder4[7:0](
+    .a_i(a_i),
+    .b_i(b_i),
+    .carry_i(carry_ii),
+    .sum_o(sum_o),
+    .carry_o(carry_oo)
+);
+
+assign carry_ii[7:1] = carry_oo[6:0];
 
 endmodule
